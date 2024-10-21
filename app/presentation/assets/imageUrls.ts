581097ff6bdfa3dd316f441deb21578c570521dc
Ceagah <carlosceagah@gmail.com>
2024-10-21 18:00:46 -0300

    feat: create image urls file

---
+++ app/presentation/assets/imageUrls.ts
@@ -0,0 +1,18 @@
+export const carouselImages = [
+  {
+    id: 1,
+    url: "https://ideogram.ai/assets/image/lossless/response/b9U8shNNQwqwM1LQcJxDag",
+  },
+  {
+    id: 2,
+    url: "https://ideogram.ai/assets/image/lossless/response/4wo1-_k3TDaohseF-swjxg",
+  },
+  {
+    id: 3,
+    url: "https://ideogram.ai/assets/image/lossless/response/ewqqEkkeTtCaZ_5E5KwiVw",
+  },
+  {
+    id: 4,
+    url: "https://ideogram.ai/assets/image/lossless/response/MorOPvfZT5OwxE4jVWvJgA",
+  },
+];

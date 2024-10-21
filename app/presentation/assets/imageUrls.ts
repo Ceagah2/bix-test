3e9d9d4a350a9792d849f24ce2ada45e952f9ed2
Ceagah <carlosceagah@gmail.com>
2024-10-21 18:08:12 -0300

    feat: create title and texts to images

--- app/presentation/assets/imageUrls.ts
+++ app/presentation/assets/imageUrls.ts
@@ -1,18 +1,30 @@
+// assets/imageUrls.ts
+
 export const carouselImages = [
   {
     id: 1,
     url: "https://ideogram.ai/assets/image/lossless/response/b9U8shNNQwqwM1LQcJxDag",
+    title: "Gerencie suas Finanças",
+    description:
+      "Mantenha o controle das suas despesas e receitas de forma prática.",
   },
   {
     id: 2,
     url: "https://ideogram.ai/assets/image/lossless/response/4wo1-_k3TDaohseF-swjxg",
+    title: "Invista com Sabedoria",
+    description: "Descubra as melhores opções de investimento para seu perfil.",
   },
   {
     id: 3,
     url: "https://ideogram.ai/assets/image/lossless/response/ewqqEkkeTtCaZ_5E5KwiVw",
+    title: "Planejamento Financeiro",
+    description:
+      "Elabore um plano financeiro que se adapte ao seu estilo de vida.",
   },
   {
     id: 4,
     url: "https://ideogram.ai/assets/image/lossless/response/MorOPvfZT5OwxE4jVWvJgA",
+    title: "Acompanhe Seus Gastos",
+    description: "Visualize seus gastos e encontre áreas para economizar.",
   },
 ];
